9ee9a6221f793e9552452ea628d16894388e312e
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-12 21:43:24 -0600

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TipmyGit::Application.config.secret_key_base = 'e1533ec44faffe596ea1c9aa302659335eebcfd3448760f4d2cdf27ef692a7092ddf530dc83045b262b691c827f470aff8e56a5820e7baa9980323e0cec2c97a'
