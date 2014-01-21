1cf89377eb967039e4625fb4d39e2f819008d8e6
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-21 15:35:45 -0600

    [#3] setup omniauth config

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,11 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  if Rails.env.production?
+    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  else
+    provider :github, '8d8f78cdd4842c37cf20', '11615684e5c7e722af1206f63d89cbfff636c409'
+  end
+
+  OmniAuth.config.on_failure = PageController.action(:homepage)
+end
