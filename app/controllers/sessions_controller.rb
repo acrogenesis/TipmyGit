265ee883567031d8f3abf0a2a60843b292d6c566
Adrian Rangel <adrian.rangel@gmail.com>
2014-01-21 21:03:30 -0600

    #3 create session oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    raise env['omniauth.auth'].to_yaml
-    #user = User.from_omniauth(env["omniauth.auth"])
-    #session[:user_id] = user.id
-    #redirect_to root_url, notice: "Signed in!"
+    #raise env['omniauth.auth'].to_yaml
+    user = User.from_omniauth(env["omniauth.auth"])
+    session[:user_id] = user.id
+    redirect_to root_url, notice: "Signed in!"
   end
 
   def destroy
